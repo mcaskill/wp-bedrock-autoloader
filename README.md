f142e5b4897a691d114e32653d5812aebe0682ce
Chauncey McAskill <shimoroka@gmail.com>
2016-04-21 14:12:15 -0400

    Bedrock 1.8.5

---
+++ README.md
@@ -0,0 +1,31 @@
+# Bedrock Autoloader for WordPress
+
+Standalone version of the Bedrock Autoloader for must-use plugins.
+
+## Installation
+
+Require this package, with [Composer](https://getcomposer.org), in the root directory of your project.
+
+```
+$ composer require mcaskill/bedrock-autoloader
+```
+
+## Usage
+
+Add [`bedrock-autoloader.php`](bedrock-autoloader.php) to your must-use plugins directory.
+
+Example:
+
+```
+<?php
+
+if ( ! is_blog_installed() ) {
+    return;
+}
+
+new \Roots\Bedrock\Autoloader(__DIR__);
+```
+
+## Credits
+
+All credits & copyrights belongs to people behind [Bedrock](https://github.com/roots/bedrock).
---
+++ bedrock-autoloader.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The autoloader bootstrap file
+ *
+ * @link    https://github.com/roots/bedrock
+ * @package Roots\Bedrock
+ *
+ * @wordpress-plugin
+ * Plugin Name: Bedrock Autoloader
+ * Plugin URI: https://github.com/roots/bedrock/
+ * Description: An autoloader that enables standard plugins to be required just like must-use plugins. The autoloaded plugins are included during mu-plugin loading. An asterisk (*) next to the name of the plugin designates the plugins that have been autoloaded.
+ * Version: 1.8.5
+ * Author: Roots
+ * Author URI: https://roots.io/
+ * License: MIT License
+ */
+
+if ( ! is_blog_installed() ) {
+    return;
+}
+
+new \Roots\Bedrock\Autoloader(__DIR__);
